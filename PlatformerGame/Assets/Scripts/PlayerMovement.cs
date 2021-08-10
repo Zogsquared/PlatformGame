865395225e92d8c72a39369fba733b60a91aa02f
Troy <shutman18@gmail.com>
2021-08-10 14:42:47 +1200

    Movement

--- PlatformerGame/Assets/Scripts/PlayerMovement.cs
+++ PlatformerGame/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+     horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+     if (Input.GetButtonDown("Jump")) {
+         jump = true;
+     }
+
+    }
+
+    void FixedUpdate () 
+    {
+
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
